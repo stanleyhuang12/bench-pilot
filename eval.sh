a59a318bd103cb28e7329c641dd6f6cc905d629c
Stanley Huang <stanleyh201k@gmail.com>
2026-03-29 12:38:20 -0400

    write brief descriptions of cli params

--- eval.sh
+++ eval.sh
@@ -3,7 +3,12 @@
 set -e 
 
 python 1-test-scenario-construction.py --num-batch 2 
+# --num-batch 3-5; we suggest having 3-5 batches to prevent hitting rate limits 
+# in config.json make sure to specify ~10 configurtions 
 
 python 2-simulation.py --b emotional-dependency --semaphore 3
+# semaphore: supports 5 concurrent coroutines 
+# --b the name of the benchmark folder 
 
 python 3-evaluation.py --b emotional-dependency 
+
